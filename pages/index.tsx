237936675f6c74b013d08b4f1c96f26e3d33f5d6
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-11 22:47:56 +0100

    feat: use idustries box component on pl main page version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
-import MainTemplate from "@components/containers/MainTemplate/MainTemplate";
+import styled from "styled-components";
+import IndustrieBox from "@components/IndustrieBox";
+import magmetBg from "src/assets/images/magmetBG.jpg";
 
 export default function Home() {
   return (
-    <h1>strona główna</h1>
+    <HomepageStyles>
+      <IndustrieBox 
+        path={"/dkice"}
+        title={"Dkice"} 
+        content={"Czyszczenie suchym lodem, czyszczenie hydrodynamiczne"}
+      />
+      <IndustrieBox
+        path={"/magmet"}
+        title={"Dkice schody & balustrady"}
+        content={"Nowocześnie stylizowane ogrodzenia, balustrady, bramy, schody kondygnacyjnych oraz wiele innych konstrukcji metalowych"}
+        bgImage={'/magmetBG.jpg'}
+      />
+    </HomepageStyles>
   )
 }
+
+const HomepageStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  padding-top: 8.4rem;
+`;
