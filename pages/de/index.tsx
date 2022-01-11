5196e9df3705e02e23a52e6b09707c4fa9c19af4
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-11 21:38:50 +0100

    feat: create basic pages structure de version

---
+++ pages/de/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const IndexPageDe = () => {
+  return (
+    <HomepageStyles>
+      <h1>Dkice & magmet homepage de</h1>
+    </HomepageStyles>
+  )
+}
+
+export default IndexPageDe;
+
+const HomepageStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  padding-top: 8.4rem;
+`;
