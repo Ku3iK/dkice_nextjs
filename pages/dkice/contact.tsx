166d6e63fb34a3d2f9a7aaf65b6e6b2d630f52d6
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-15 18:25:42 +0100

    Feat  create contact page for dkice (#13)
    * feat: create contact page for dkice
    
    * feat: create contact page for dkice data

--- pages/dkice/contact.tsx
+++ pages/dkice/contact.tsx
@@ -1,6 +1,23 @@
+import TitleWithDescription from '@components/TitleWithDescription';
+import contactPageData from 'src/assets/i18n/dkice/contactPage.json';
+import formData from 'src/assets/i18n/contactForms.json';
+import Form from '@components/Form';
+
+const { contactPage } = contactPageData;
+const { contactForms } = formData;
+
 const DkiceContact = () => {
   return (
-    <h1>Dkice contact</h1>
+    <>
+        <TitleWithDescription content={contactPage} />
+        <Form 
+          formName={'dkiceContact'} 
+          isNetlifyFrom 
+          formContent={contactForms} 
+          withoutContainer={true}
+          buttonText={'Wyślij'}
+        />
+    </>
   )
 }
 
