9747e162837e0a0e3476c6f96e187ae9e1d2f637
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-15 18:18:02 +0100

    feat: create form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,39 @@
+import Button from '@components/Buttons/Button';
+import { FormWrapper } from './Form.styled';
+import FormFieldsList from './FormFieldsList';
+import { FormInterface } from './FormInterface';
+import { useRouter } from 'next/router';
+
+const Form = ({
+    formName,
+    isNetlifyFrom,
+    formContent,
+    withoutContainer,
+    buttonText,
+}: FormInterface) => {
+    const { locale } = useRouter();
+
+    return(
+        <FormWrapper withoutContainer={withoutContainer && withoutContainer}>
+            <form
+                name={formName && formName} 
+                method={'POST'} 
+                data-netlify={isNetlifyFrom} 
+                className='flex flex-col justify-center'
+            >   
+                {formContent?.filter(({ language }: any) => language === locale )?.map((({ fields }: any, key: number) => (
+                    <FormFieldsList 
+                        {...{ key: key }}
+                        formContent={fields} 
+                    />
+                )))
+                }
+                <div>
+                    {buttonText && <Button type={'submit'}>{buttonText}</Button>}
+                </div>
+            </form>
+        </FormWrapper>
+    );
+};
+
+export default Form;
