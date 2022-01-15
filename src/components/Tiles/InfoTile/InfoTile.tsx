6603b7945d48bf5a1ae886e1f302fb6deb63d1ed
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-15 18:27:40 +0100

    Feat  create info tile component (#22)
    * feat: create info tile component
    
    * feat: create info tile component styles
    
    * feat: create info tile component interface
    
    * feat: create info tile component index file

---
+++ src/components/Tiles/InfoTile/InfoTile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { InfoTileWrapper } from './InfoTile.styled';
+import { InfoTileInterface } from './InfoTileInterface';
+
+const InfoTile = ({ 
+    title, 
+    values,
+}: InfoTileInterface) => {
+    return (
+        <InfoTileWrapper 
+            {...{largeSize: values?.length > 150 ? true : false}}
+            className={'flex flex-col grow p-24'}
+        >
+            <h4 className={'pb-16 text-24 font-semibold'}>{title}</h4>
+            {values?.map(({ value }, key: number) => (
+                <p 
+                    key={key}
+                    className={'pb-8 text-16'}
+                >
+                    {value}
+                </p>
+            ))}
+        </InfoTileWrapper>
+    );
+};
+
+export default InfoTile;
