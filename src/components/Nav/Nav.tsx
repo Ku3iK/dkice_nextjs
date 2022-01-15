c89fed0b1e098928081806590553323026655477
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-15 18:27:29 +0100

    ref: use tailwind to styled navigation (#21)

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -16,9 +16,9 @@ const Nav =  ({
         <NavWrapper {...{isNavVisible: isNavVisible}}>
             <Container className={"nav"}>
                 <Link href='/'>DKice</Link>
-                <ul>
+                <ul className={'flex'}>
                     {routes && routes.map(( {path, name }, pathKey ) => (
-                        <li key={pathKey}>
+                        <li key={pathKey} className={'p-18'}>
                             <Link 
                                 href={path} 
                                 {...{activeClassName: 'active'}} 
@@ -28,9 +28,14 @@ const Nav =  ({
                         </li>
                     ))}
                 </ul>
-                <div className="nav__burgerMenu">
+                <div className={`nav__burgerMenu flex z-50 py-24`}>
                     {routes && (
-                        <div className="nav__burgerMenu-burger" onClick={() => setNavVisibility(!isNavVisible)} role="button" tabIndex={0}>
+                        <div 
+                            className={`nav__burgerMenu-burger flex flex-col justify-between w-20 h-12 mr-2 opacity-100 cursor-pointer`} 
+                            onClick={() => setNavVisibility(!isNavVisible)} 
+                            role="button" 
+                            tabIndex={0}
+                        >
                             <div></div>
                             <div></div>
                             <div></div>
