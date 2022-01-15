9811a50497a58d4b7fc9934a9055b9f0662b98fc
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-15 18:12:56 +0100

    feat: create title with description component

---
+++ src/components/TitleWithDescription/TitleWithDescription.tsx
@@ -0,0 +1,36 @@
+import InfoTile from '../Tiles/InfoTile/InfoTile';
+import { TitleWithDescriptionWrapper } from './TitleWithDescription.styled';
+import { useRouter } from 'next/router';
+import { InfoTileInterface } from '@components/Tiles/InfoTile/InfoTileInterface';
+
+const TitleWithDescription = ({ content }: any) => {
+    const { locale } = useRouter();
+
+    return (
+        <>
+            {content?.filter(({ language }: any) => language === locale )?.map(({ pageTitle, pageDescription, infoTiles }: any, key: number) => (
+                <TitleWithDescriptionWrapper 
+                    key={key}
+                    className={'mb-48 text-center md:mt-0 md:mx-auto md:mb-64'}
+                >
+                    {pageTitle && <h1 className={'pb-24 text-32 font-bold'}>{pageTitle}</h1>}
+                    {pageDescription && <p className={'pb-48'}>{pageDescription}</p>}
+                    {infoTiles?.length > 0 && (
+                        <div className={'flex justify-between flex-wrap gap-32 text-left'}>
+                            { infoTiles.map(({ title, values, isLink }: InfoTileInterface, tileKey: number) => (
+                                <InfoTile 
+                                    key={tileKey}
+                                    title={title}
+                                    values={values}
+                                    isLink={isLink}
+                                />
+                            ))}
+                        </div>
+                    )}
+                </TitleWithDescriptionWrapper>
+            ))}
+        </>
+    );
+};
+
+export default TitleWithDescription;
