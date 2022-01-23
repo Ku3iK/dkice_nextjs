e774e15ebcb8ac987970df6b2f14b0993966ef81
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-23 19:48:17 +0100

    Fix  improve navigation performance (#29)
    * feat: create paths transtaltions
    
    * feat: create domain regex
    
    * feat: add new props in nav interface
    
    * fix: change link href
    
    * fix: rendering of navigation elements depending on the selected page

--- src/components/containers/MainTemplate/MainTemplate.tsx
+++ src/components/containers/MainTemplate/MainTemplate.tsx
@@ -1,12 +1,25 @@
 import Nav from "@components/Nav";
-import Container from "../Container/Container";
+import { useRouter } from 'next/router';
+import pathTranslationsContent from 'src/assets/i18n/pathTranslations.json';
+import { dkiceDomainRegex } from 'src/utils/domainRegex';
+
+const { pathTranslations }: any = pathTranslationsContent;
 
 const MainTemplate: React.FunctionComponent = ({
   children,
 }: any) => {
+
+  const { locale, asPath }: any = useRouter();
+  const domain = dkiceDomainRegex.test(asPath) ? 'dkice' : 'magmet';
+  const lang = pathTranslations?.filter(({ language }: any) => language === locale);
+  const isMainPage = asPath === '/de-DE/' || asPath === '/';
+
   return (
     <>
-      <Nav />
+      <Nav 
+        domain={domain}
+        routes={!isMainPage && lang[0].pathTranslation}
+      />
         <main>
           {children}
         </main>
---
+++ src/utils/domainRegex.tsx
@@ -0,0 +1,1 @@
+export const dkiceDomainRegex = /^\/dkice.*/;
