c2960da98a1e4523275d97f00bdca47a0eeaab58
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-24 22:08:15 +0100

    Feat  create single image component (#30)
    * feat: create single image component
    
    * feat: create single image component interface

---
+++ src/components/Gallery/SingleImage/SingleImageInterface.tsx
@@ -0,0 +1,9 @@
+export interface SingleImageInterface {
+    index?: number;
+    image: any;
+    imageTitle?: string;
+    imageTitleDe?: string;
+    imageDescription?: string;
+    imageDescriptionDe?: string;
+    handleSelectDisplayedImg?: any;
+}
