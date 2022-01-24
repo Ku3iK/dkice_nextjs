9b2d42246363adc16edcf6386606bc898c38f107
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-24 22:08:30 +0100

    Feat  create gallery image list (#31)
    * feat: create gallery image list
    
    * feat: create gallery image list interface
    
    * feat: create gallery image list

---
+++ src/components/Gallery/GalleryImageList/GalleryImagesList.tsx
@@ -0,0 +1,37 @@
+import SingleImage from '@components/Gallery/SingleImage/SingleImage';
+import { GalleryImageListInterface, ImageListInterface } from '@components/Gallery/GalleryImageList/GalleryImageListInterface';
+import { useRouter } from 'next/router';
+
+const GalleryImagesList = ({
+    imagesList,
+    onImageClick,
+    isSlider,
+}: GalleryImageListInterface) => {
+    const { locale }: any = useRouter();
+
+    return (
+        <>
+            {imagesList?.length > 0 && (
+                imagesList.map((
+                    { 
+                        id,
+                        image, 
+                        imageTitle, 
+                        imageDescription, 
+                        imageTitleDe, 
+                        imageDescriptionDe 
+                    }: ImageListInterface, key: number) => (
+                        <SingleImage
+                            {...{ key: id }}
+                            image={image}
+                            imageTitle={locale === "pl-PL" ? imageTitle : imageTitleDe}
+                            imageDescription={locale === "pl-PL" ? imageDescription : imageDescriptionDe}
+                            {...!isSlider && { handleSelectDisplayedImg: onImageClick, index: key }}
+                        />
+                ))
+            )}
+        </>
+    )
+};
+
+export default GalleryImagesList;
