458ba84dde37fb2607102d696a6320487c067e3f
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-26 20:40:54 +0100

    fix: edit homepage of dkcie and use hero component

--- pages/dkice/index.tsx
+++ pages/dkice/index.tsx
@@ -1,16 +1,31 @@
 import Container from '@components/containers/Container/Container';
-import useOfDryIceCleaningContent from 'src/assets/i18n/dkice/useOfDryIceCleaning.json';
+import dkiceHomePageContent from 'src/assets/i18n/dkice/dkiceHomePage.json';
 import DynamicContent from '@components/containers/DynamicContent';
+import Hero from '@components/Hero';
+import { useRouter } from 'next/router';
+import React from 'react';
 
-const { tilesContainer }: any = useOfDryIceCleaningContent.useOfDryIceCleaning[0];
+const { dkiceHomePage }: any = dkiceHomePageContent;
 
 const DkicePage = () => {
+  const { locale } = useRouter();
 
   return (
     <Container>
-      <DynamicContent 
-        tilesContainer={tilesContainer}
-      />
+      {dkiceHomePage && dkiceHomePage?.filter(({ language }: any) => language === locale ).map(({ heroTitle, heroDescription, heroImageUrl, buttonData, tilesContainer }: any, key: number) => (
+        <React.Fragment key={key}>
+          <Hero 
+            title={heroTitle}
+            description={heroDescription}
+            imageUrl={heroImageUrl}
+            buttonData={buttonData}
+          />
+          {console.log(tilesContainer)}
+          <DynamicContent 
+            tilesContainer={tilesContainer[0]}
+          />
+        </React.Fragment>
+      ))}
     </Container>
   )
 }
