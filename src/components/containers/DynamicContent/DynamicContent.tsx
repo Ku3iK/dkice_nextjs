a7623838248c44c7c0d23249629ca858d12a068d
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-26 18:58:44 +0100

    fix: use text from file

--- src/components/containers/DynamicContent/DynamicContent.tsx
+++ src/components/containers/DynamicContent/DynamicContent.tsx
@@ -13,7 +13,9 @@ const DynamicContent = ({
 
     return(
         <div>
-            <h3 className={'pb-48'}>Branże, gdzie można zastosować czyszczenie (piaskowanie) suchym lodem</h3>
+            {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ sectionTitle }, sectionTitleKey: number) => (
+                <h3 className={'pb-48'}>{sectionTitle}</h3>
+            ))}
             <DynamicContentStyled className={'grid xs:grid-cols-2 gap-32'}>
                 <div className={'flex overflow-x-scroll xs:overflow-visible xs:grid xs:grid-cols-1 lg:grid-cols-2 gap-32'}>
                 {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ tiles }, tilesKey: number) => (
@@ -35,8 +37,9 @@ const DynamicContent = ({
                 ))}
                 </div>
                 <div>
-                    {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ tiles }) => (
+                    {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ tiles }, tilesKey: number) => (
                         <DynamicContentText 
+                            {...{key: tilesKey}}
                             listElements={tiles[activeTile].listElements}
                         />
                     ))}
