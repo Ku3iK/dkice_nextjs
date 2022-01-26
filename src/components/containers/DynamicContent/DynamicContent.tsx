37da66c23bf4d8e17c51494de95f6a1b174566fc
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-26 20:44:04 +0100

    Fix  edit dynamic content component (#48)
    * fix: edit dynamic content component interface structure
    
    * fix: edit dynamic component props

--- src/components/containers/DynamicContent/DynamicContent.tsx
+++ src/components/containers/DynamicContent/DynamicContent.tsx
@@ -1,26 +1,22 @@
-import IconWithListTile from "@components/Tiles/IconWithListTile";
-import React, { useState } from "react";
+import IconWithListTile from '@components/Tiles/IconWithListTile';
+import { useState } from 'react';
 import { DynamicContentStyled } from './DynamicContent.styled';
-import { DynamicContentInterface } from "./DynamicContentInterface";
-import DynamicContentText from "./DynamicContentText";
-import { useRouter } from "next/router";
+import { DynamicContentInterface } from './DynamicContentInterface';
+import DynamicContentText from './DynamicContentText';
 
 const DynamicContent = ({
     tilesContainer,
 }: DynamicContentInterface) => {
-    const { locale } = useRouter();
     const [ activeTile, setActiveTile ] = useState(0);
 
     return(
-        <div>
-            {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ sectionTitle }, sectionTitleKey: number) => (
-                <h3 className={'pb-48'}>{sectionTitle}</h3>
-            ))}
+        <div id="dynamicContent">
+            <h3 className={'pb-48'}>{tilesContainer.sectionTitle}</h3>
             <DynamicContentStyled className={'grid xs:grid-cols-2 gap-32'}>
                 <div className={'flex overflow-x-scroll xs:overflow-visible xs:grid xs:grid-cols-1 lg:grid-cols-2 gap-32 py-32 md:py-0'}>
-                {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ tiles }, tilesKey: number) => (
-                    <React.Fragment key={tilesKey}>
-                        {tiles?.map(({ iconUrl, tileTitle, listElements }, contentKey: number) => (
+                {tilesContainer && (
+                    <>
+                        {tilesContainer.tiles?.map(({ iconUrl, tileTitle, listElements }, contentKey: number) => (
                             <div
                                 key={contentKey}
                                 onClick={() => setActiveTile(contentKey)}
@@ -32,17 +28,16 @@ const DynamicContent = ({
                                 />
                             </div>
                         ))}
-                    </React.Fragment>
+                    </>
 
-                ))}
+                )}
                 </div>
                 <div>
-                    {tilesContainer?.filter(({ language }: any) => language === locale ).map(({ tiles }, tilesKey: number) => (
+                    {tilesContainer && (
                         <DynamicContentText 
-                            {...{key: tilesKey}}
-                            listElements={tiles[activeTile].listElements}
+                            listElements={tilesContainer.tiles[activeTile].listElements}
                         />
-                    ))}
+                    )}
                 </div>
             </DynamicContentStyled>
         </div>
