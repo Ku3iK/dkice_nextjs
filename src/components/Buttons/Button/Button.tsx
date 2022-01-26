910bd09192ab0211d13121a9c26dac2fcd226d30
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-26 20:44:16 +0100

    fix: set option to enable button scroll in button component (#49)

--- src/components/Buttons/Button/Button.tsx
+++ src/components/Buttons/Button/Button.tsx
@@ -3,14 +3,25 @@ import classNames from 'clsx';
 
 const Button = ({ 
     children, 
-    type
+    type,
+    notCentered,
+    as,
+    scrollTo,
 }: any) => {
-    const classess = classNames(`block my-0 mx-auto bg-transparent py-12 px-24 text-16 text-grey cursor-pointer`);
+    const classess = classNames(
+        `block my-0 bg-transparent py-12 px-24 text-16 text-grey cursor-pointer`,
+        {
+            'mx-auto': !notCentered,
+            'inline-block': as,
+        }
+    );
 
     return (
         <ButtonWrapper 
+            href={as === 'a' && scrollTo && scrollTo}
             type={type && type}
             className={classess}
+            as={as && scrollTo && as}
         >
             {children}
         </ButtonWrapper>
