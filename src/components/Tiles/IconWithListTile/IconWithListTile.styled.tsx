95665b07d9b4abbdf774ba74641910e8c185ec4c
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-26 19:06:45 +0100

    fix: edit dynamic content styles (#45)

--- src/components/Tiles/IconWithListTile/IconWithListTile.styled.tsx
+++ src/components/Tiles/IconWithListTile/IconWithListTile.styled.tsx
@@ -4,12 +4,15 @@ export const IconWithListTileStyled = styled.div`
     min-height: 20rem;
     max-height: 30rem;
     border-radius: 2.5rem;
+    height: 100%;
+    min-width: 20rem;
     
     @media (min-width: ${({ theme }) => theme.screens.sm}) {
         -webkit-box-shadow: 16px 15px 56px -28px rgba(49, 48, 45, 1);
         -moz-box-shadow: 16px 15px 56px -28px rgba(49, 48, 45, 1);
         box-shadow: 16px 15px 56px -28px rgba(49, 48, 45, 1);
         transition: transform .4s ease-in-out;
+        min-width: auto;
     }
 
     &:hover {
