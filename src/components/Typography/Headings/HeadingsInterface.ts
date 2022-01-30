08167fcc2658702e1cf6c111a7eca11ac8702cb7
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-01-30 15:11:13 +0100

    feat: create headings component interface

---
+++ src/components/Typography/Headings/HeadingsInterface.ts
@@ -0,0 +1,9 @@
+import { HTMLAttributes } from 'react';
+
+export interface HeadingsInterface {
+    level?: 1 | 2 | 3 | 4 | 5 | 6 | 'subtitle';
+    classHeading?: 1 | 2 | 3 | 4 | 5 | 6 | 'subtitle';
+    boldText?: boolean;
+}
+
+export type HeadingElementProps = HTMLAttributes<HTMLElement>
