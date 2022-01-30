3adf9678ff796aa884dec712823f0011cc9807b9
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-01-30 15:14:51 +0100

    Feat  create magmet homepage (#51)
    * feat: create magmet homepage content file
    
    * feat: use hero with image in background in magmet homepage

--- pages/magmet/index.tsx
+++ pages/magmet/index.tsx
@@ -1,8 +1,27 @@
-import * as React from "react"
+import magmetHomePageContent from 'src/assets/i18n/magmet/magmetHomePage.json';
+import { useRouter } from 'next/router';
+import Container from '@components/containers/Container/Container';
+import React from 'react';
+import BgImageHero from '@components/BgImageHero';
+
+const { magmetHomePage }: any = magmetHomePageContent;
 
 const MagmetPage = () => {
+  const { locale } = useRouter();
+
   return (
-    <h1>Dkice schody & balustrady</h1>
+    <div className='pt-80'>
+      {magmetHomePage && magmetHomePage?.filter(({ language }: any) => language === locale ).map(({ heroTitle, heroDescription, heroImageUrl, buttonData }: any, key: number) => (
+        <React.Fragment key={key}>
+            <BgImageHero 
+              title={heroTitle}
+              description={heroDescription}
+              imageUrl={heroImageUrl}
+              buttonData={buttonData}
+            />
+        </React.Fragment>
+      ))}
+    </div>
   )
 }
 
