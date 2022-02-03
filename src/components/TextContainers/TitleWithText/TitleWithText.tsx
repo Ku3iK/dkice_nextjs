7c6a426846ce0d3b5f6b3ba869c24cafac6ff365
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-03 21:10:00 +0100

    feat: create title with texts component

---
+++ src/components/TextContainers/TitleWithText/TitleWithText.tsx
@@ -0,0 +1,32 @@
+import Container from '@components/containers/Container/Container';
+import Headings from '@components/Typography/Headings';
+import Paragraphs from '@components/Typography/Paragraphs';
+import React from 'react';
+import { TitleWithTextInterface } from './TitleWithTextInterface';
+
+const TitleWithText: React.FunctionComponent<TitleWithTextInterface> = ({
+    isTitleInOtherColumn,
+    title,
+    texts,
+    classNames,
+}) => {
+    return(
+        <section className={classNames}>
+            <Container className={'grid md:grid-cols-2 gap-48 pt-0'} {...{withoutPadding: true}}>
+                {isTitleInOtherColumn && (
+                    <div>
+                        <Headings level={2}>{title}</Headings>
+                    </div>
+                )}
+                <div>
+                    { !isTitleInOtherColumn && <Headings level={2}>{title}</Headings> }
+                    {texts?.map(({ text }, textKey: number) => (
+                        <Paragraphs key={textKey} className='pb-16'>{text}</Paragraphs>
+                    ))}
+                </div>
+            </Container>
+        </section>
+    );
+}
+
+export default TitleWithText;
