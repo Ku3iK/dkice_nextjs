213137e8716c12a12f2d6ad9a88b7677d4173566
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-02-08 20:30:56 +0100

    feat: add container to bg image hero component (#69)

--- src/components/BgImageHero/BgImageHero.tsx
+++ src/components/BgImageHero/BgImageHero.tsx
@@ -1,4 +1,5 @@
 import Button from '@components/Buttons/Button';
+import Container from '@components/containers/Container/Container';
 import { HeroInterface } from '@components/Hero/HeroInterface';
 import Headings from '@components/Typography/Headings';
 import Paragraphs from '@components/Typography/Paragraphs';
@@ -13,41 +14,43 @@ const BgImageHero = ({
 }: HeroInterface) => {
     return(
         <BgImageHeroStyled className={'relative'}>
-            <div className={'w-full md:w-2/3 xl:w-1/2 px-32 py-48 z-50 text-textLight'}>
-                {title && 
-                    <Headings 
-                        level={1} 
-                        boldText
-                    >
-                        {title}
-                    </Headings>}
-                {description && 
-                    <Paragraphs 
-                        level={'medium'}
-                        className={'py-32'}
-                    >
-                        {description}
-                    </Paragraphs>}
-                {buttonData && (
-                    <div>
-                        <Button 
-                            scrollTo={buttonData.href && buttonData.href}
-                            notCentered
-                            as={'a'}
+            <Container {...{withoutPadding: true}}>
+                <div className={'w-full md:w-2/3 xl:w-1/2 px-32 py-48 z-50 text-textLight'}>
+                    {title && 
+                        <Headings 
+                            level={1} 
+                            boldText
                         >
-                            {buttonData.buttonContent}
-                        </Button>
-                    </div>
-                )}
-            </div>
-            <div className="absolute -z-10 top-0 right-0 bottom-0 left-0">
-                <Image
-                    src={imageUrl}
-                    layout={'fill'}
-                    objectFit={'cover'}
-                    quality={100}
-                />
-            </div>
+                            {title}
+                        </Headings>}
+                    {description && 
+                        <Paragraphs 
+                            level={'medium'}
+                            className={'py-32'}
+                        >
+                            {description}
+                        </Paragraphs>}
+                    {buttonData && (
+                        <div>
+                            <Button 
+                                scrollTo={buttonData.href && buttonData.href}
+                                notCentered
+                                as={'a'}
+                            >
+                                {buttonData.buttonContent}
+                            </Button>
+                        </div>
+                    )}
+                </div>
+                <div className="absolute -z-10 top-0 right-0 bottom-0 left-0">
+                    <Image
+                        src={imageUrl}
+                        layout={'fill'}
+                        objectFit={'cover'}
+                        quality={100}
+                    />
+                </div>
+            </Container>
         </BgImageHeroStyled>
     );
 }
