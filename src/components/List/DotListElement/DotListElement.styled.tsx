78e5fdcd412117b15de4adf5175295b1740f1c09
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-09 17:44:02 +0100

    fix: edit dot list element styles on mobile devices

--- src/components/List/DotListElement/DotListElement.styled.tsx
+++ src/components/List/DotListElement/DotListElement.styled.tsx
@@ -6,6 +6,8 @@ export const DotListElementStyled = styled.li`
         display: block;
         width: 1rem;
         height: 1rem;
+        min-width: 1rem;
+        min-height: 1rem;
         margin-right: 1.2rem;
         background-color: ${({ theme }) => theme.color.black};
         border-radius: 25rem;
