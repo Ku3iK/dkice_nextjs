6284a6d577588ea73cdfdfac8336d19a6d04172c
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-09 21:34:08 +0100

    feat: create lists container

---
+++ src/components/containers/ListsContainer/ListsContainer.tsx
@@ -0,0 +1,36 @@
+import List from '@components/List';
+import Headings from '@components/Typography/Headings';
+import Paragraphs from '@components/Typography/Paragraphs';
+import Container from '../Container/Container';
+import { ListsContainerInterface } from './ListContainerInterface';
+
+export const ListsContainer:React.FunctionComponent<ListsContainerInterface> = ({
+    title,
+    lists,
+}) => {
+    return(
+        <section>
+            <Container {...{withoutPadding: true}}>
+                {title && (
+                    <Headings level={2}>{title}</Headings>
+                )}
+                <div className={'grid xlg:grid-cols-2 gap-32 xlg:gap-48 pt-32 md:pt-48'}>
+                    {lists?.map(({ listDescription, listContent }, listKey:number) => (
+                        <div key={listKey}>
+                            {listDescription && (
+                                <Paragraphs level={'medium'}>{listDescription}</Paragraphs>
+                            )}
+                            {listContent && (
+                                <List 
+                                    listContent={listContent}
+                                />
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </section>
+    );
+}
+
+export default ListsContainer;
