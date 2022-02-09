20e3232e386a50b688e3a831bb3fbee9196fb22d
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-09 19:09:53 +0100

    feat: add 'what is htdronomy cleanins section for dkice homepage

--- pages/dkice/index.tsx
+++ pages/dkice/index.tsx
@@ -8,6 +8,7 @@ import TitleWithText from '@components/TextContainers/TitleWithText';
 import AccordionSection from '@components/AccordionSection';
 import TextsGrid from '@components/TextContainers/TextsGrid';
 import TextWithList from '@components/TextContainers/TextWithList';
+import TextWithImage from '@components/TextContainers/TextWithImage';
 
 const { dkiceHomePage }: any = dkiceHomePageContent;
 
@@ -26,7 +27,8 @@ const DkicePage = () => {
         principlesOfDryIceCleaning,
         dryIceApplication,
         cleanlinssInEveryField,
-        hydronomyCleaning
+        hydronomyCleaning,
+        whatIsHydronomyCleaning
       }: any, key: number) => (
         <React.Fragment key={key}>
           <BgImageHero 
@@ -40,36 +42,37 @@ const DkicePage = () => {
               isTitleInOtherColumn={whatIsDryIceCleaningSection.isTitleInOtherColumn}
               title={whatIsDryIceCleaningSection.title}
               texts={whatIsDryIceCleaningSection.texts}
-              classNames="py-48 md:py-80"
+              classNames={'firstSection'}
             />
             <AccordionSection 
               reverseElements
               title={principlesOfDryIceCleaning.title}
               description={principlesOfDryIceCleaning.description}
               tabs={principlesOfDryIceCleaning.accordionTabs}
-              classNames="pb-48 md:pb-80"
             />
             <TextsGrid
               title={dryIceApplication.title}
               texts={dryIceApplication.texts}
-              classNames="py-48 md:py-80"
             />
             <AccordionSection 
               title={cleanlinssInEveryField.title}
               description={cleanlinssInEveryField.description}
               tabs={cleanlinssInEveryField.accordionTabs}
-              classNames="py-48 md:py-80"
             />
             <DynamicContent 
               tilesContainer={tilesContainer[0]}
-              classNames="py-48 md:py-80"
             />
             <TextWithList
               title={hydronomyCleaning.title}
               description={hydronomyCleaning.description}
               listTitle={hydronomyCleaning.listTitle}
               listContent={hydronomyCleaning.listContent}
-              classNames="py-48 md:py-80"
+            />
+            <TextWithImage 
+              title={whatIsHydronomyCleaning.title}
+              texts={whatIsHydronomyCleaning.texts}
+              imageUrl={whatIsHydronomyCleaning.imageUrl}
+              reverseElements={whatIsHydronomyCleaning.reverseElements}
             />
           </Container>
         </React.Fragment>
