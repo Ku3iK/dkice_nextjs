77c78584fbf180f144a254bc1889590823bfb743
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-16 22:32:23 +0100

    feat: create footer address component

---
+++ src/components/Footer/FooterAddress/FooterAddress.tsx
@@ -0,0 +1,18 @@
+import Paragraphs from "@components/Typography/Paragraphs";
+import { FooterAddressInterface } from "../FooterInterface";
+
+const FooterAddress = ({ companyAddress }: FooterAddressInterface ) => {
+    return (
+        <div>
+            {companyAddress && (
+                <div>
+                    {Object.keys(companyAddress).map((value, valueIndex) => (
+                        <Paragraphs key={valueIndex}>{companyAddress[value]}</Paragraphs>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default FooterAddress;
