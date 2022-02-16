222ade59ea2e9802de018d921c81dc881deedda1
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-16 22:32:07 +0100

    feat: create footer contact component

---
+++ src/components/Footer/FooterContact/FooterContact.tsx
@@ -0,0 +1,29 @@
+import Paragraphs from "@components/Typography/Paragraphs";
+import { FooterContactStyled } from "../Footer.styled";
+import { FooterContactInterface } from "../FooterInterface";
+
+const renderLink = ( link: string,  linkPrefix?: 'tel' | 'mailto' | '#' ) => (
+    <a href={`${linkPrefix && linkPrefix}:${link}`}>{link}</a>
+);
+
+const FooterContact = ({
+    title,
+    emails,
+    phones
+}: FooterContactInterface) => {
+    return (
+        <FooterContactStyled>
+            {title && (
+                <Paragraphs level={'medium'}>{title}</Paragraphs>
+            )}
+            {emails && emails?.map(({  mail }, mailIndex: number) => (
+                <Paragraphs key={mailIndex}>{renderLink(mail, 'mailto')}</Paragraphs>
+            ))}
+            {phones && phones?.map(({ name, phone }, phoneIndex: number) => (
+                <Paragraphs key={phoneIndex}>{name}: {renderLink(phone, 'tel')}</Paragraphs>
+            ))}
+        </FooterContactStyled>
+    );
+}
+
+export default FooterContact;
