0879b9429dabecd3b44107b677f76d81dde632f9
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-02-16 22:31:54 +0100

    feat: create footer nav component

---
+++ src/components/Footer/FooterNav/FooterNav.tsx
@@ -0,0 +1,33 @@
+import Headings from "@components/Typography/Headings";
+import Link from "next/link";
+import { FooterNavInterface } from "../FooterInterface";
+
+const FooterNav = ({
+    navigationTitle,
+    domain,
+    paths,
+}: FooterNavInterface) => {
+    return (
+        <div>
+            {navigationTitle && (
+                <Headings level={4}>{navigationTitle}</Headings>
+            )}
+            <ul className={'pt-16'}>
+                {paths.map(( {path, name }, pathKey ) => (
+                    <li 
+                        key={pathKey} 
+                        className={'py-6 text-textLight'} 
+                    >
+                        <Link 
+                            href={`/${domain}${path}`}
+                        >
+                                {name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default FooterNav;
