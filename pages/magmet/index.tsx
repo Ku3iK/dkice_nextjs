a3c1eb5ca1783b50d56f7f4b936028583af6222f
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-02-27 13:43:32 +0100

    Feat  add how stairs are made section to magmet homepage (#99)
    * feat: add how stairs are made images
    
    * feat: add how stairs are made content
    
    * feat: add how stairs are made content to magmet homepage

--- pages/magmet/index.tsx
+++ pages/magmet/index.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import BgImageHero from '@components/BgImageHero';
 import TitleWithText from '@components/TextContainers/TitleWithText';
 import TextWithList from '@components/TextContainers/TextWithList';
+import AlternateSection from '@components/AlternateSection';
 
 const { magmetHomePage }: any = magmetHomePageContent;
 
@@ -12,45 +13,55 @@ const MagmetPage = () => {
   const { locale } = useRouter();
 
   return (
-    <div className='pt-80'>
-      {!!magmetHomePage?.length && (magmetHomePage?.filter(({ language }: any) => language === locale ).map(({ 
-        heroTitle, 
-        heroDescription, 
-        heroImageUrl, 
-        buttonData,
-        aboutStairs,
-        whatYouWin,
-        aboutTerraces,
-      }: any, key: number) => (
-        <React.Fragment key={key}>
-            <BgImageHero 
-              title={heroTitle}
-              description={heroDescription}
-              imageUrl={heroImageUrl}
-              buttonData={buttonData}
-            />
-            <Container {...{insideSection: true}}>
-              <TitleWithText 
-                isTitleInOtherColumn={aboutStairs.isTitleInOtherColumn}
-                title={aboutStairs.sectionTitle}
-                texts={aboutStairs.texts}
-                classNames={'firstSection'}
-              />
-              <TextWithList 
-                title={whatYouWin.title}
-                description={whatYouWin.description}
-                listContent={whatYouWin.listContent}
-              />
-              <TitleWithText 
-                isTitleInOtherColumn={aboutTerraces.isTitleInOtherColumn}
-                title={aboutTerraces.sectionTitle}
-                texts={aboutTerraces.texts}
-              />
-            </Container>
-        </React.Fragment>
-      )))}
+    <div className="pt-80">
+      {!!magmetHomePage?.length &&
+        magmetHomePage
+          ?.filter(({ language }: any) => language === locale)
+          .map(
+            (
+              {
+                heroTitle,
+                heroDescription,
+                heroImageUrl,
+                buttonData,
+                aboutStairs,
+                whatYouWin,
+                aboutTerraces,
+                howStairsAreMade
+              }: any,
+              key: number
+            ) => (
+              <React.Fragment key={key}>
+                <BgImageHero
+                  title={heroTitle}
+                  description={heroDescription}
+                  imageUrl={heroImageUrl}
+                  buttonData={buttonData}
+                />
+                <Container {...{ insideSection: true }}>
+                  <TitleWithText
+                    isTitleInOtherColumn={aboutStairs.isTitleInOtherColumn}
+                    title={aboutStairs.sectionTitle}
+                    texts={aboutStairs.texts}
+                    classNames={'firstSection'}
+                  />
+                  <TextWithList
+                    title={whatYouWin.title}
+                    description={whatYouWin.description}
+                    listContent={whatYouWin.listContent}
+                  />
+                  <TitleWithText
+                    isTitleInOtherColumn={aboutTerraces.isTitleInOtherColumn}
+                    title={aboutTerraces.sectionTitle}
+                    texts={aboutTerraces.texts}
+                  />
+                  <AlternateSection title={howStairsAreMade.title} steps={howStairsAreMade.steps} />
+                </Container>
+              </React.Fragment>
+            )
+          )}
     </div>
-  )
-}
+  );
+};
 
 export default MagmetPage;
