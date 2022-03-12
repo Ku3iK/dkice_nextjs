879e25542fd7316ae586e92ecad79818e15dd9ea
Ku3iK <47992953+Ku3iK@users.noreply.github.com>
2022-03-12 13:19:51 +0100

    Fix  edit dkice homepage (#102)
    * fix: edit dkice homepage
    
    * feat: add image to magmet homepage
    
    * fix: change text with list component and set option to display image

--- src/components/TextContainers/TextWithList/TextWithList.tsx
+++ src/components/TextContainers/TextWithList/TextWithList.tsx
@@ -3,41 +3,47 @@ import List from '@components/List';
 import Headings from '@components/Typography/Headings';
 import Paragraphs from '@components/Typography/Paragraphs';
 import { TextWithListInterface } from './TextWithListInterface';
+import Image from 'next/image';
 
 const TextWithList: React.FunctionComponent<TextWithListInterface> = ({
-    title,
-    description,
-    listTitle,
-    listContent,
-    classNames
+  title,
+  description,
+  listTitle,
+  listContent,
+  imageUrl,
+  classNames
 }) => {
-    return (
-        <section className={`${classNames}`}>
-            <Container {...{withoutPadding: true}}>
-                {title && (
-                    <Headings level={2}>{title}</Headings>
-                )}
-                <div className={'grid md:grid-cols-2 gap-32 md:gap-48'}>
-                    {description && (
-                        <div>
-                            <Paragraphs 
-                                level={'medium'}
-                                className={'py-32'}
-                            >
-                                {description}
-                            </Paragraphs>
-                        </div>
-                    )}
-                    <div className={'md:py-32'}>
-                        {listTitle && (
-                            <Headings level={5}>{listTitle}</Headings>
-                        )}
-                        <List listContent={listContent} />
-                    </div>
+  return (
+    <section className={`${classNames}`}>
+      <Container {...{ withoutPadding: true }}>
+        {title && <Headings level={2}>{title}</Headings>}
+        <div className={'grid md:grid-cols-2 gap-32 md:gap-48'}>
+          {description && (
+            <div className="flex flex-col">
+              <Paragraphs level={'medium'} className={'py-32'}>
+                {description}
+              </Paragraphs>
+              {imageUrl && (
+                <div className={'relative h-[100vw] max-h-[400px]'}>
+                  <Image
+                    src={imageUrl}
+                    alt={''}
+                    layout={'fill'}
+                    objectFit={'cover'}
+                    className={'rounded-16'}
+                  />
                 </div>
-            </Container>
-        </section>
-    );
-}
+              )}
+            </div>
+          )}
+          <div className={'md:py-32'}>
+            {listTitle && <Headings level={5}>{listTitle}</Headings>}
+            <List listContent={listContent} />
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
 
 export default TextWithList;
--- src/components/TextContainers/TextWithList/TextWithListInterface.ts
+++ src/components/TextContainers/TextWithList/TextWithListInterface.ts
@@ -1,11 +1,12 @@
-import { TextsArrayType } from "../TextContainersInterface";
+import { TextsArrayType } from '../TextContainersInterface';
 
 export interface TextWithListInterface {
-    title?: string;
-    description?: string;
-    listTitle?: string;
-    listContent: Array<{
-        listElement: string;
-    }>;
-    classNames?: string;
+  title?: string;
+  description?: string;
+  listTitle?: string;
+  imageUrl?: string;
+  listContent: Array<{
+    listElement: string;
+  }>;
+  classNames?: string;
 }
