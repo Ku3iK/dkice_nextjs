383728541ba915ebc791384180974273f1752027
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-03-15 20:45:03 +0100

    fix: use static props

--- pages/dkice/gallery.tsx
+++ pages/dkice/gallery.tsx
@@ -17,7 +17,7 @@ const DKICE_GALLERY_QUERY = `
   }
 `;
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   //@ts-ignore
   const data = await request({
     query: DKICE_GALLERY_QUERY
