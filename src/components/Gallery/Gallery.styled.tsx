260461788584b4841a8a07ecf342a87f25797ca3
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-03-15 20:38:08 +0100

    fix: change styles for single image component

--- src/components/Gallery/Gallery.styled.tsx
+++ src/components/Gallery/Gallery.styled.tsx
@@ -1,41 +1,50 @@
 import styled from 'styled-components';
 
 export const GallerySliderWrapper = styled.div`
-    background-color: rgba(0, 0, 0, .7);
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 export const GallerySliderContent = styled.div`
-    width: 60%; 
+  width: 60%;
 `;
 
 export const ExitButton = styled.button`
-    left: 105%;
-    transform: translate(0, -50%);
-    z-index: 99;
-    transition: transform .3s ease-in-out;
+  left: 105%;
+  transform: translate(0, -50%);
+  z-index: 99;
+  transition: transform 0.3s ease-in-out;
 
-    &:hover {
-        transform: translate(0, -50%) scale(1.2);
-    }
+  &:hover {
+    transform: translate(0, -50%) scale(1.2);
+  }
 `;
 
 export const SingleImageWrapper = styled.div`
-    transition: ease-in-out .3s;
+  height: 25rem;
+  transition: ease-in-out 0.3s;
 
-    & > div {
-        background-color: rgba(0, 0, 0, .4);
-        transition: ease-in-out .3s;
+  &.singleImageInSlider {
+    height: 30vw;
+    margin: 0 auto;
+    min-height: 40rem;
+    max-height: 80rem;
+    max-width: 70%;
+  }
 
-        & > * {
-            width: 100%;
-        }
+  & > div {
+    background-color: rgba(0, 0, 0, 0.4);
+    transition: ease-in-out 0.3s;
 
-        p {
-            -moz-hyphens:auto;
-            -ms-hyphens:auto;
-            -webkit-hyphens:auto;
-            hyphens:auto;
-            word-wrap:break-word;
-        }
+    & > * {
+      width: 100%;
     }
+
+    p {
+      -moz-hyphens: auto;
+      -ms-hyphens: auto;
+      -webkit-hyphens: auto;
+      hyphens: auto;
+      word-wrap: break-word;
+    }
+  }
 `;
