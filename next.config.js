0fabc8e19845ae2f34640c280bf7eeab1b8af9d1
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-03-15 20:38:22 +0100

    feat: add images domains to next config file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.datocms-assets.com']
+  },
   i18n: {
-   locales: ["pl-PL", "de-DE"],
-   defaultLocale: "pl-PL",
- },
-}
+    locales: ['pl-PL', 'de-DE'],
+    defaultLocale: 'pl-PL'
+  }
+};
