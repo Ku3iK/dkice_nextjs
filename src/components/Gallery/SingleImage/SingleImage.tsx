e1dd3657ece57c7abbebefc0e553b5742b935ecc
Jakub Kaczmarek <kubakaczmrek1102@gmail.com>
2022-03-15 20:37:53 +0100

    fix: change className for single image

--- src/components/Gallery/SingleImage/SingleImage.tsx
+++ src/components/Gallery/SingleImage/SingleImage.tsx
@@ -1,28 +1,33 @@
+import clsx from 'clsx';
+import Image from 'next/image';
 import { SingleImageWrapper } from '../Gallery.styled';
 import { SingleImageInterface } from './SingleImageInterface';
 
 const SingleImage = ({
-    index,
-    image,
-    imageTitle,
-    imageDescription,
-    handleSelectDisplayedImg,
+  index,
+  image,
+  imageTitle,
+  imageDescription,
+  handleSelectDisplayedImg,
+  className
 }: SingleImageInterface) => {
-    return (
-        <SingleImageWrapper 
-            className={`relative cursor-pointer`}
-            onClick={handleSelectDisplayedImg && (() => handleSelectDisplayedImg(index))} >
-            <img 
-                src={image.url} 
-                alt={imageTitle ? imageTitle : `Zdjęcie nr ${index} w galerii`} 
-                className={`w-full h-full`}
-            />
-            <div className={`flex flex-col absolute left-0 top-0 w-full h-full overflow-y-scroll opacity-0 py-16 px-24 text-textLight hover:opacity-100`}>
-                {imageTitle && <h3 className={`pb-24 text-24`}>{imageTitle}</h3>}
-                {imageDescription && <p className={`text-16`}>{imageDescription}</p>}
-            </div>
-        </SingleImageWrapper>
-    );
+  return (
+    <SingleImageWrapper
+      className={clsx(`relative cursor-pointer`, className)}
+      onClick={handleSelectDisplayedImg && (() => handleSelectDisplayedImg(index))}>
+      <Image
+        src={image.url}
+        alt={imageTitle ? imageTitle : `Zdjęcie nr ${index} w galerii`}
+        layout="fill"
+        objectFit="cover"
+      />
+      <div
+        className={`flex flex-col absolute left-0 top-0 w-full h-full overflow-y-scroll opacity-0 py-16 px-24 text-textLight hover:opacity-100`}>
+        {imageTitle && <h3 className={`pb-24 text-24`}>{imageTitle}</h3>}
+        {imageDescription && <p className={`text-16`}>{imageDescription}</p>}
+      </div>
+    </SingleImageWrapper>
+  );
 };
 
 export default SingleImage;
